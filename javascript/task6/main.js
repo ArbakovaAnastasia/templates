a891c3046911e8874be24d325fa1e382969c23ab
Anastasia Arbakova <arbakova.nastya@gmail.com>
2025-04-15 00:21:43 +0800

    add

--- javascript/task6/main.js
+++ javascript/task6/main.js
@@ -4,3 +4,42 @@
 // после чего проходить по массиву users и искать в нем объект с такими же значениями.
 // Если пользователь найден в массиве users, то программа должна поздороваться с ним по имени. 
 // В противном случае должна появиться ошибка авторизации.
+
+const USERS = [
+    {
+        name: 'admin',
+        login: 'admin',
+        password: 'admin'
+    },
+    {
+        name: 'nastya',
+        login: 'nastya',
+        password: '123'
+    },
+    {
+        name: 'billie',
+        login: 'billie',
+        password: '456'
+    }
+];
+
+let userFound = false;
+
+while (!userFound) {
+    const inputLogin = prompt('Введите логин', '');
+    const inputPassword = prompt('Введите пароль', '');
+    
+    const user = USERS.find(user => user.login === inputLogin && user.password === inputPassword);
+
+    if (user) {
+        userFound = true;
+        alert(`Привет, ${user.name}!`);
+    } else {
+        alert('Неверный логин или пароль. Попробуйте еще раз.');
+    }
+}
+
+
+
+
+
