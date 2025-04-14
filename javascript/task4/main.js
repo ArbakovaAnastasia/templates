ad555281aad62133cfc3ffd87e7b3b6a9c003561
Anastasia Arbakova <arbakova.nastya@gmail.com>
2025-04-14 22:23:10 +0800

    add

--- javascript/task4/main.js
+++ javascript/task4/main.js
@@ -7,3 +7,71 @@
 // если пользователь нажимает “Отмена”, то игра заканчивается.
  
 // буквы кирилица и латиница, пробелы, 123фыв, 123asd, фыв123, asd123, символы, дробные
+
+let randomNumber;
+let userNumber;
+let mark_guess = false;
+let playAgain = true;
+
+while (playAgain) {
+    randomNumber = setRandomNumber();
+    mark_guess = false;
+
+    while (mark_guess == false) {
+        inputNumber();
+        mark_guess = compareNumbers(randomNumber, userNumber);
+    }
+    playAgain = confirm('Сыграем еще раз?');
+}
+
+
+//---
+function setRandomNumber() {
+    let randomNum = Math.round(Math.random() * 10);
+    isNotNull(randomNum);
+    console.log(randomNum);
+    alert('Я загадал число от 1 до 10! Угадай!');
+    return randomNum;
+}
+
+function isNotNull(number) {
+    if (typeof number === 'undefined') {
+        return;
+    }
+    if (number == 0) setRandomNumber();
+}
+
+function inputNumber() {
+    userNumber = prompt('Введи число', '');
+    while (isNumeric(userNumber) == false) {
+        userNumber = prompt('Некорректный ввод, напишите заново. Введи число', '');
+    }
+}
+
+function isNumeric(number) {
+    if (typeof number === 'undefined') {
+        return;
+    }
+
+    let functionResult = false;
+    if (number.replace(/\s/g, "").length === 0 || isNaN(number) || !Number.isInteger(Number(number))) {
+        functionResult = false;
+        alert("Некорректный ввод, напишите заново");
+    } else {
+        functionResult = true;
+        alert(`Число ${number} принято`);
+    }
+    return functionResult;  
+}
+
+function compareNumbers(randomNumber, userNumber) {
+    if (randomNumber == userNumber) {
+        alert('Ты выиграл!');
+        return true;
+    } else if (randomNumber < userNumber) {
+        alert('Меньше!');
+    } else if (randomNumber > userNumber) {
+        alert('Больше!');
+    }
+    return false;
+}
